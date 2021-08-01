f3fa3f03acc7490afee0f4afd4026ea47073d5c7
jonnielrubia <jonnielrubia@gmail.com>
2021-08-01 11:27:51 +0800

    navbar

--- js/setting.js
+++ js/setting.js
@@ -1,8 +1,10 @@
 let ball =document.querySelector('.ball');
+let bar = document.querySelector('.nav-icon1')
+let navbar = document.querySelector('.nav-bar')
+let navLink = document.querySelector('.nav-bar-link');
 
 function buttonchangecolor(){
     if (document.querySelector('input[type="checkbox"]:checked')){
-    
         document.querySelector('.ball').classList.add('active');
         document.querySelector('body').classList.add('bgcolor');
 
@@ -10,6 +12,10 @@ function buttonchangecolor(){
         document.querySelector('.ball').classList.remove('active');
         document.querySelector('body').classList.remove('bgcolor');
     }
-        
-    
+}
+
+function bars(){
+    bar.classList.toggle('open');
+    navbar.classList.toggle('active-mobile-nav')
+    navLink.classList.toggle('active-mobile-link');
 }
