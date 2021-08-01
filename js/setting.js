8323947deb2aff320e434380dc29bfbb130d6ea8
jonnielrubia <jonnielrubia@gmail.com>
2021-08-01 20:49:34 +0800

    face animation

--- js/setting.js
+++ js/setting.js
@@ -3,6 +3,20 @@ let bar = document.querySelector('.nav-icon1')
 let navbar = document.querySelector('.nav-bar')
 let navLink = document.querySelector('.right-nav-bar');
 
+
+document.querySelector('body').addEventListener('mousemove',eyeball);
+
+function eyeball(){
+    let eye = document.querySelectorAll('.eye');
+    eye.forEach(function (eye){
+        let x = (eye.getBoundingClientRect().left) + (eye.clientWidth / 2);
+        let y = (eye.getBoundingClientRect().top) + (eye.clientHeight / 2);
+        let radian = Math.atan2(event.pageX - x, event.pageY - y);
+        let rot = (radian * (180 / Math.PI) * -1 ) + 270;
+        eye.style.transform = "rotate("+ rot +"deg)"; 
+    });
+}
+
 function buttonchangecolor(){
     if (document.querySelector('input[type="checkbox"]:checked')){
         document.querySelector('.ball').classList.add('active');
