c1ea5af39964dec17c4be3dfcff13fcd3e9bfedc
jonnielrubia <jonnielrubia@gmail.com>
2021-08-06 17:22:50 +0800

    update

--- js/setting.js
+++ js/setting.js
@@ -2,14 +2,27 @@ let ball =document.querySelector('.ball');
 let navbar = $('.nav-bar')
 let navLink = $('.right-nav-bar');
 let mybutton = document.getElementById("myBtn");
-let loader = $('.spinner-border');
 
-window.onscroll = ()=>{
-    scrollFunction()
-};
+// window.onscroll = ()=>{
+//     scrollFunction()
+// };
+
 
 $(document).ready(()=>{
-    loader.fadeOut();
+    $('.spinner-border').fadeOut();
+
+    $(document).on('scroll',()=>{
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+          } else {
+            mybutton.style.display = "none";
+          }
+    })
+
+    $('#myBtn').on('click',()=>{
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    })
 
     $('.nav-icon1').on('click',()=>{
         $('.nav-icon1').toggleClass('open');
@@ -21,22 +34,22 @@ $(document).ready(()=>{
     $('.face').animate({
         opacity: 1
     },{
-        duration: 800
+        duration: 800,
     });
+
+    $('body').on('mousemove',()=>{
+        let eye = document.querySelectorAll('.eye');
+        eye.forEach(function (eye){
+            let x = (eye.getBoundingClientRect().left) + (eye.clientWidth / 2);
+            let y = (eye.getBoundingClientRect().top) + (eye.clientHeight / 2);
+            let radian = Math.atan2(event.pageX - x, event.pageY - y);
+            let rot = (radian * (180 / Math.PI) * -1 ) + 270;
+            eye.style.transform = "rotate("+ rot +"deg)"; 
+        });
+    })
 })
 
-document.querySelector('body').addEventListener('mousemove',eyeball);
 
-function eyeball(){
-    let eye = document.querySelectorAll('.eye');
-    eye.forEach(function (eye){
-        let x = (eye.getBoundingClientRect().left) + (eye.clientWidth / 2);
-        let y = (eye.getBoundingClientRect().top) + (eye.clientHeight / 2);
-        let radian = Math.atan2(event.pageX - x, event.pageY - y);
-        let rot = (radian * (180 / Math.PI) * -1 ) + 270;
-        eye.style.transform = "rotate("+ rot +"deg)"; 
-    });
-}
 
 function buttonchangecolor(){
     if (document.querySelector('input[type="checkbox"]:checked')){
@@ -52,16 +65,11 @@ function buttonchangecolor(){
 // function bars(){
 // }
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      mybutton.style.display = "block";
-    } else {
-      mybutton.style.display = "none";
-    }
-}
+// function scrollFunction() {
+// }
 
 
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
